f700503a7ba8bda00d29ce0ef959b5dc7cf2a8be
azizbeksenior <sadinovazizbe@gmail.com>
2023-09-06 18:23:09 +0500

    New End

--- src/Components/End/End.jsx
+++ src/Components/End/End.jsx
@@ -8,19 +8,19 @@ export default function End() {
   return (
     <div className="End">
         <div className="End_icons">
-            <FaFacebookF id='icon'/>
-            <AiOutlineTwitter id='icon'/>
-            <AiFillYoutube id='icon'/>
-            <AiFillInstagram id='icon'/>
+            <a href="https://www.facebook.com/Pixar/" target='_blank'><FaFacebookF id='icon'/></a>
+            <a href="https://twitter.com/pixar" target='_blank'><AiOutlineTwitter id='icon'/></a>
+            <a href="https://www.youtube.com/pixar" target='_blank'><AiFillYoutube id='icon'/></a>
+            <a href="https://www.instagram.com/pixar" target='_blank'><AiFillInstagram id='icon'/></a>
         </div>
         <div className="End_1">
-            <a href="">PRIVACY POLICY</a> <span>|</span>
-            <a href="">TERMS OF USE</a> <br />
-            <a href="">YOUR US STATE PRIVACY RIGHTS</a><span>|</span>
-            <a href="">CHILDREN’S ONLINE PRIVACY POLICY</a> <br />
-            <a href="">INTEREST-BASED ADS  </a> <br /> <br />
-            <p>© 1986-2023 DISNEY / PIXAR</p> <br />
-            <a href=""><button>Do Not Sell or Share My Personal Information</button></a>
+            <a href="https://privacy.thewaltdisneycompany.com/en/" target='_blank'>PRIVACY POLICY</a> <span>|</span>
+            <a href="https://disneytermsofuse.com/" target='_blank'>TERMS OF USE</a> <br />
+            <a href="https://privacy.thewaltdisneycompany.com/en/current-privacy-policy/your-us-state-privacy-rights/" target='_blank'>YOUR US STATE PRIVACY RIGHTS</a><span>|</span>
+            <a href="https://privacy.thewaltdisneycompany.com/en/for-parents/childrens-online-privacy-policy/" target='_blank'>CHILDREN’S ONLINE PRIVACY POLICY</a> <br />
+            <a href="http://preferences-mgr.truste.com/?affiliateId=115&type=disneycolor" target='_blank'>INTEREST-BASED ADS  </a> <br /> <br />
+            <p>© 1986-2023 DISNEY / PIXAR</p> <br /> <br /><br />
+            <a href="" id='a1'>Do Not Sell or Share My Personal Information</a>
         </div>
     </div>
   )
